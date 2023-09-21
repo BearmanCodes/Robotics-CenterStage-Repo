02c4dcc08c41e9452277b6a827a3f48c71613cf8
BearmanCodes <idostuff1225@gmail.com>
2023-09-21 16:35:59 -0400

    21, September, 2023
    Added some color and servo stuff

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servo.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.exception.RobotCoreException;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "ServoTest")
+public class servo extends LinearOpMode {
+    Servo frogo;
+    Gamepad currentGamepad = new Gamepad();
+    Gamepad previousGamepad = new Gamepad();
+    double servoPos;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        frogo = hardwareMap.get(Servo.class, "frogo");
+        //Start pos: 0.85
+        waitForStart();
+        while (opModeIsActive()){
+            try {
+                edgeDetector();
+            } catch (RobotCoreException e) {
+                e.printStackTrace();
+            }
+            if (currentGamepad.a && !previousGamepad.a){ //Change to A once I get back
+                servoPos += 0.05;
+                telemetry.addData("servoPos: ", servoPos);
+                telemetry.addData("currentPos: ", frogo.getPosition());
+                telemetry.update();
+            }
+            if (currentGamepad.b && !previousGamepad.b){ //Change to A once I get back
+                servoPos -= 0.05;
+                telemetry.addData("servoPos: ", servoPos);
+                telemetry.addData("currentPos: ", frogo.getPosition());
+                telemetry.update();
+            }
+            if (currentGamepad.y && !previousGamepad.y){
+                frogo.setPosition(servoPos);
+                telemetry.addData("servoPos", servoPos);
+                telemetry.addData("currentPos", frogo.getPosition());
+                telemetry.update();
+            }
+        }
+    }
+
+    public void edgeDetector() throws RobotCoreException {
+        previousGamepad.copy(currentGamepad);
+        currentGamepad.copy(gamepad2);
+    }
+}
