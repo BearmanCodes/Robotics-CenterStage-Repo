af40e593445df9d54758f1edf6e9232781a9c520
BearmanCodes <idostuff1225@gmail.com>
2023-11-06 16:51:37 -0500

    KNOW THIS WORKS
    BadaBoom

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShawnDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShawnDrive.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.exception.RobotCoreException;
@@ -8,30 +7,13 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
-import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 @TeleOp
 public class ShawnDrive extends LinearOpMode {
     DcMotorEx frontleft, frontright, backleft, backright, armMotor;
 
     Servo lClaw, rClaw;
-
-    IMU imu;
-    IMU.Parameters imuparams;
-
-    YawPitchRollAngles robotOrientation;
-
-    AngularVelocity myRobotAngularVelocity;
-
-
-    static final double TicksPerRev = 560;
-    static final double WheelInches = (75 / 25.4);
-    static final double TicksPerIn = TicksPerRev / (WheelInches * Math.PI);
 
     public double reducerArm = 0.4;
     public double armPower;
@@ -71,7 +53,7 @@ public class ShawnDrive extends LinearOpMode {
         } catch (RobotCoreException e) {
             throw new RuntimeException(e);
         }
-        if(currentGamepad2.a && !previousGamepad2.a){
+        if(currentGamepad.a && !previousGamepad.a){
             clawstat = !clawstat;
             if(clawstat){
                 rClaw.setPosition(0.12);
@@ -83,31 +65,38 @@ public class ShawnDrive extends LinearOpMode {
         }
     }
 
+    public void allMotorPower(double power){
+        frontright.setPower(power);
+        frontleft.setPower(power);
+        backleft.setPower(power);
+        backright.setPower(power);
+    }
+
     public void main(){
         double Vertical = gamepad1.left_stick_y;
         double Horizontal = gamepad1.left_stick_x * 1.2;
         double Pivot = gamepad1.right_stick_x;
 
-        double frontleftPower = (-Pivot + (Vertical - Horizontal)) * reducer;
-        double frontrightPower = (Pivot + Vertical + Horizontal) * reducer;
-        double backrightPower = (Pivot + (Vertical - Horizontal)) * reducer;
-        double backleftPower = (-Pivot + Vertical + Horizontal) * reducer;
-
-        if (currentGamepad.right_bumper && !previousGamepad.right_bumper){
-            Turn();
-        }
-
-        /*Brake code Klarissa added*/  while (gamepad1.circle){
+        double frontLeftPower = (-Pivot + (Vertical - Horizontal)) * reducer;
+        double frontRightPower = (Pivot + Vertical + Horizontal) * reducer;
+        double backRightPower = (Pivot + (Vertical - Horizontal)) * reducer;
+        double backLeftPower = (-Pivot + Vertical + Horizontal) * reducer;
+        /*Brake code Klarissa added  while (gamepad1.circle){
             allMotorPower(0);
 
-            //make go back
-
+            make go back
+            if (gamepad1.right_bumper){
+                frontleft.setDirection(DcMotorSimple.Direction.REVERSE);
+                frontright.setDirection(DcMotorSimple.Direction.FORWARD);
+                backleft.setDirection(DcMotorSimple.Direction.REVERSE);
+                backright.setDirection(DcMotorSimple.Direction.REVERSE);
+            }
         }
-        
-        frontleft.setPower(frontleftPower);
-        frontright.setPower(frontrightPower);
-        backleft.setPower(backleftPower);
-        backright.setPower(backrightPower);
+        */
+        frontleft.setPower(frontLeftPower);
+        frontright.setPower(frontRightPower);
+        backleft.setPower(backLeftPower);
+        backright.setPower(backRightPower);
 
     }
 
@@ -126,14 +115,6 @@ public class ShawnDrive extends LinearOpMode {
 
                 .build();
 
-        imu = hardwareMap.get(IMU.class, "botIMU");
-        imuparams = new IMU.Parameters(new RevHubOrientationOnRobot
-                (RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                        RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
-
-        imu.initialize(imuparams);
-        imu.resetYaw();
-        
         frontleft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontright.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backleft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -153,87 +134,8 @@ public class ShawnDrive extends LinearOpMode {
         lClaw.setPosition(0.23);
     }
 
-    public void Turn() {
-        if (opModeIsActive()) {
-            robotOrientation = imu.getRobotYawPitchRollAngles();
-            double Yaw = robotOrientation.getYaw(AngleUnit.DEGREES);
-            double targetPos = Yaw - 180;
-            if (opModeIsActive()) {
-                while (Yaw > targetPos) {
-                    setMotorVelocity(300, -300, 300, -300);
-                    robotOrientation = imu.getRobotYawPitchRollAngles();
-                    Yaw = robotOrientation.getYaw(AngleUnit.DEGREES);
-                    telemetry.addData("Yaw:", "%.2f", Yaw);
-                    telemetry.update();
-                }
-                allMotorVelocity(0);
-                if (Yaw < targetPos) {
-                    robotOrientation = imu.getRobotYawPitchRollAngles();
-                    Yaw = robotOrientation.getYaw(AngleUnit.DEGREES);
-                    double error = Yaw - -90;
-                    while (Math.abs(error) > 0.2) {
-                        setMotorVelocity(-300, 300, -300, 300);
-                        robotOrientation = imu.getRobotYawPitchRollAngles();
-                        Yaw = robotOrientation.getYaw(AngleUnit.DEGREES);
-                        error = Yaw - 90;
-                        telemetry.addData("Error: ", "%.2f", error);
-                        telemetry.addData("Yaw:", "%.2f", Yaw);
-                        telemetry.update();
-                    }
-                    allMotorVelocity(0);
-                }
-            }
-        }
-    }
-
-    public void allMotorMode(DcMotor.RunMode mode) {
-        frontleft.setMode(mode);
-        frontright.setMode(mode);
-        backleft.setMode(mode);
-        backright.setMode(mode);
-    }
-
-    public void allMotorVelocity(double velocity) {
-        frontleft.setVelocity(velocity);
-        frontright.setVelocity(velocity);
-        backleft.setVelocity(velocity);
-        backright.setVelocity(velocity);
-    }
-
-    public void setMotorVelocity(double fLvelocity, double fRvelocity, double bLvelocity, double bRvelocity) {
-        frontleft.setVelocity(fLvelocity);
-        frontright.setVelocity(fRvelocity);
-        backleft.setVelocity(bLvelocity);
-        backright.setVelocity(bRvelocity);
-    }
-
-    public void allMotorPower(double power){
-        frontleft.setPower(power);
-        frontright.setPower(power);
-        backleft.setPower(power);
-        backright.setPower(power);
-    }
-
-    public void setMotorPower(double frontleftPower, double frontrightPower, double backleftPower,
-                              double backrightPower){
-        frontleft.setPower(frontleftPower);
-        frontright.setPower(frontrightPower);
-        backleft.setPower(backleftPower);
-        backright.setPower(backrightPower);
-    }
-
-    public void allTargetPosition(int frontleftPos, int frontrightPos,
-                                  int backleftPos, int backrightPos){
-        frontleft.setTargetPosition(frontleftPos);
-        frontright.setTargetPosition(frontrightPos);
-        backleft.setTargetPosition(backleftPos);
-        backright.setTargetPosition(backrightPos);
-    }
-
     public void edgeDetector() throws RobotCoreException {
         previousGamepad.copy(currentGamepad);
         currentGamepad.copy(gamepad1);
-        previousGamepad2.copy(currentGamepad2);
-        currentGamepad2.copy(gamepad2);
     }
 }
