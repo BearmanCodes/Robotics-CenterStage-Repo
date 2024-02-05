cd1a894dd9b4a76b0c3976e298933ec4f81e14b5
BearmanCodes <idostuff1225@gmail.com>
2024-02-05 17:25:55 -0500

    Updated tuning constants, needs more work.
    Help me

--- road-runner-quickstart-quickstart1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ road-runner-quickstart-quickstart1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -35,8 +35,8 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
-      getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(24.7653, 0, 10.3654,
+      getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV)); //11.7025
 
     /*
      * These are physical constants that can be determined from your robot (including the track
